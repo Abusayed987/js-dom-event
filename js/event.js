aa673d72078eff4623002939463b83d1246816d1
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-24 12:19:50 +0600

    Different ways to use onclick

--- js/event.js
+++ js/event.js
@@ -1,1 +1,25 @@
 console.log('welcome to our new event');
+
+function darkMode() {
+    document.body.style.backgroundColor= 'black';
+}
+
+function lightMood() {
+    document.body.style.backgroundColor= 'white';
+}
+
+function redMood(){
+    document.body.style.backgroundColor= 'red';
+}
+
+const makeBlueMode = document.getElementById('blue');
+makeBlueMode.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor= 'blue';
+}
+
+const makePurple =  document.getElementById('purple')
+makePurple.onclick = function makaPurple(){
+    document.body.style.backgroundColor= 'purple'
+}
